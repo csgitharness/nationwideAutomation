4181e7cdc25d5108276ed2734a5c97bd102abbfa
CustomerSuccess <52584620+csgitharness@users.noreply.github.com>
2019-12-18 14:41:41 +0530

    Add files via upload

---
+++ cloud_provider_generator.sh
@@ -0,0 +1,38 @@
+
+CLOUD_PROVIDER_NAME=""
+SERVICE_ACCOUNT=""
+
+fn_cloud_provider(){
+touch $CLOUD_PROVIDER_NAME.yaml 
+cat << EOF  > ${CLOUD_PROVIDER_NAME}.yaml  
+harnessApiVersion: '1.0'
+type: KUBERNETES_CLUSTER
+masterUrl: https://35.247.123.240
+serviceAccountToken: ${SERVICE_ACCOUNT}
+skipValidation: false
+usageRestrictions:
+  appEnvRestrictions:
+  - appFilter:
+      filterType: ALL
+    envFilter:
+      filterTypes:
+      - PROD
+  - appFilter:
+      filterType: ALL
+    envFilter:
+      filterTypes:
+      - NON_PROD
+useKubernetesDelegate: false
+EOF
+
+}
+
+### MAIN ####
+
+ cd Cloud\ Providers/
+
+CLOUD_PROVIDER_NAME=$1
+SERVICE_ACCOUNT=$2
+
+echo "Creating Cloud Provider"
+fn_cloud_provider
