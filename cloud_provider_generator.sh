1414562cc09eaec4fb1e9251c05f055e7148ea2e
Rohan Gupta <rohan.gupta@harness.io>
2019-12-27 02:24:17 -0500

    cloud providers generation rough draft complete

--- cloud_provider_generator.sh
+++ cloud_provider_generator.sh
@@ -1,43 +1,40 @@
 
 CLOUD_PROVIDER_NAME=""
 SERVICE_ACCOUNT=""
+ENV_TYPE=""
+APP_NAME=""
 
 #prod_namespace
 #test_namespace 
 # add app name for scope - appFilter section
 
 fn_cloud_provider(){
-touch $CLOUD_PROVIDER_NAME.yaml 
-cat << EOF  > ${CLOUD_PROVIDER_NAME}.yaml  
-harnessApiVersion: '1.0'
-type: KUBERNETES_CLUSTER
-masterUrl: https://35.247.123.240
-serviceAccountToken: ${SERVICE_ACCOUNT}
-skipValidation: false
-usageRestrictions:
-  appEnvRestrictions:
-  - appFilter:
-      filterType: ALL
-    envFilter:
-      filterTypes:
-      - PROD
-  - appFilter:
-      filterType: ALL
-    envFilter:
-      filterTypes:
-      - NON_PROD
-useKubernetesDelegate: false
-EOF
+cd Setup/Cloud\ Providers/
 
+touch $CLOUD_PROVIDER_NAME.yaml 
+
+cp  Namespace\ dpt\ -\ Prod\ Cluster\ --\ referenceCloudProvider.yaml $CLOUD_PROVIDER_NAME.yaml 
+
+echo "serviceAccount input"
+yq w $CLOUD_PROVIDER_NAME.yaml 'serviceAccountToken' $SERVICE_ACCOUNT --inplace
+
+echo "Selecting the application to scope to the cloud provider"
+yq w $CLOUD_PROVIDER_NAME.yaml 'usageRestrictions.appEnvRestrictions.[0].appFilter.entityNames[0]' $APP_NAME --inplace
+
+echo "Cloud Provider Environment Scoping Restrictions for the application"
+echo "PROD or NON_PROD"
+ls
+pwd
+yq w $CLOUD_PROVIDER_NAME.yaml 'usageRestrictions.appEnvRestrictions.[0].envFilter.filterTypes[0]' $ENV_TYPE --inplace
 }
 
 ### MAIN ####
 
- cd Setup/Cloud\ Providers/
-
 CLOUD_PROVIDER_NAME=$1
 SERVICE_ACCOUNT=$2
+APP_NAME=$3
+ENV_TYPE=$4
 
 echo "Creating Cloud Provider"
-echo "Usage: ./cloud_provider_generator.sh <CLOUD_PROVIDER_NAME> <SERVICE_ACCOUNT> "
+echo "Usage: ./cloud_provider_generator.sh <CLOUD_PROVIDER_NAME> <SERVICE_ACCOUNT> <APP_NAME> <ENV_TYPE> "
 fn_cloud_provider
