c1e5a393956d167149521728cbf15e0030bc9731
Rohan Gupta <rohan.gupta@harness.io>
2019-12-27 21:10:00 -0500

    enforcing the number of arguments passed

--- master.sh
+++ master.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+### Variables ###
 CLOUD_PROVIDER_NAME=$1
 MASTER_URL=$2
 APP_NAME=$3
@@ -9,7 +10,19 @@ INPUT_SERVICE=$5
 PROD_NAMESPACE=$6
 TEST_NAMESPACE=$7
 
+### MAIN ###
+
+
+
+
 echo "USAGE: <CLOUD_PROVIDER_NAME> <MASTER_URL> <APP_NAME> <ENV_TYPE> <INPUT_SERVICE> <PROD_NAMESPACE> <TEST_NAMESPACE>"
+
+if [ $# -lt 7 ]; then
+echo "ERROR: Not enough arguments"
+echo "USAGE: <CLOUD_PROVIDER_NAME> <MASTER_URL> <APP_NAME> <ENV_TYPE> <INPUT_SERVICE> <PROD_NAMESPACE> <TEST_NAMESPACE>"
+exit 0
+fi
+
 echo "INFO: Creating Cloud Provider"
 echo "INFO: Running Cloud Provider Script"
 ./cloud_provider_generator.sh $1 $2 $3 $4
